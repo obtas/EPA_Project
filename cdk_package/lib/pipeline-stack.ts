675e8d0509e1c559028ac6b677b091d8e60449f9
Satta Foday <samilafo@amazon.com>
2023-12-21 11:13:15 +0000

    added additional stage and configured alpha and prod stage

--- cdk_package/lib/pipeline-stack.ts
+++ cdk_package/lib/pipeline-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
-import { AlphaStage } from './pipeline-alpha-stage';
-import { BetaStage } from "./pipeline-beta-stage";
+import { ProdStage } from './pipeline-prod-stage';
+import { AlphaStage } from "./pipeline-alpha-stage";
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
@@ -35,15 +35,12 @@ export class QwizPipelineStack extends cdk.Stack {
             })
         });
 
-        const alpha_stage = pipeline.addStage(new AlphaStage(this, "Alpha", {
-            env: { account: '937836275043', region: 'us-west-2'}
+        const alpha_stage = pipeline.addStage(new AlphaStage(this, "Beta", {
+            env: { account: '522253859401', region: 'us-west-2'}
         }));
 
-
-        // const beta_stage = pipeline.addStage(new BetaStage(this, "Beta", {
-        //     env: { account: '042538542222', region: 'eu-west-2'}
-        // }));
-
- 
+        const prod_stage = pipeline.addStage(new ProdStage(this, "Alpha", {
+            env: { account: '937836275043', region: 'us-west-2'}
+        }));
     }
 }
