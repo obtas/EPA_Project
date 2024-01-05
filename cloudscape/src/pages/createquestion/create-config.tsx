e7c55dcf28eaebe892300ddf8d0d0c1e13f1be09
Satta Foday <samilafo@amazon.com>
2024-01-05 17:54:30 +0000

    create config file for create question page

---
+++ cloudscape/src/pages/createquestion/create-config.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { HelpPanel } from '@cloudscape-design/components';
+
+export const LEVEL_OPTIONS = [
+    { value: "first", label: "3" },
+    { value: "second", label:"4" },
+    { value: "third", label: "5" },
+    { value: "fourth", label: "6" },
+]
+
+export const ROLE_OPTIONS = [
+    { value: "first", label: "Systems Development Engineer" },
+    { value: "second", label:"Systems Engineer" },
+    { value: "third", label: "Support Engineer" },
+    { value: "fourth", label: "System Admin" },
+    { value: "fifth", label: "Systems Analyst" },
+]
+
+export const QUESTION_TYPE_OPTIONS = [
+    { value: "first", label: "Networking" },
+    { value: "second", label:"System Design" },
+    { value: "third", label: "Coding" },
+    { value: "fourth", label: "Learn and Be Curious" },
+    { value: "fifth", label: "Ownership" },
+    { value: "sixth", label:"Invent and simplify" },
+    { value: "seventh", label: "Are right a lot" },
+    { value: "eighth", label: "Hire and develop the best" },
+    { value: "ninth", label: "Insist on highest standards" },
+    { value: "tenth", label:"Think Big" },
+    { value: "eleventh", label: "Bias For Action" },
+    { value: "twelfth", label: "Frugality" },
+    { value: "thirteenth", label: "Earn Trust" },
+    { value: "fourteenth", label:"Dive Deep" },
+    { value: "fifteenth", label: "Have Backbone, disagree and commit" },
+    { value: "sixteenth", label: "Deliver Results" },
+    { value: "seventeenth", label: "Strive to be Earth's Best Employer" },
+    { value: "eighteenth", label:"Success and Scale Bring Broad Responsibility" },
+]
+
